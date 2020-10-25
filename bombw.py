f236de0eaf4f4b2a79350df062228a1196b31f38
devlop8299 <73418925+devlop8299@users.noreply.github.com>
2020-10-25 12:50:35 +0530

    Add files via upload

---
+++ bombw.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+driver = webdriver.Chrome('chromedriver.exe')
+driver.get('https://web.whatsapp.com/')
+driver.maximize_window()
+sleep(3)
+
+name = input("enter the username:")
+mssg =input ('enter messg to send:')
+number=int(input("enter the count:"))
+
+input('Enter any key after scanning QR code')
+user = driver.find_element_by_xpath("//span[@title = '{}']".format(name))
+user.click()
+
+for i in range(number):
+    msg_box=driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
+    msg_box.send_keys(mssg)
+    button = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button/span")
+    button.click()
+    
+print("success")    
+#driver.close()
